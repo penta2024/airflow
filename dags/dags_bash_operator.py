674ce42ee3ac757d1ba23853395006160f03b9ca
penta2024 <nandari777@penta.co.kr>
2024-05-14 16:18:20 +0900

    bash_operator dag 업로드

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -18,7 +18,7 @@ with DAG(
       
       bash_t2 = BashOperator(
         task_id="bash_t2",
-        bash_command="echo whoami",
+        bash_command="echo $HOSTNAME",
     )       
 
 bash_t1 >> bash_t2
