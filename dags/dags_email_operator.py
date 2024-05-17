8e03fc8b9e278c90b40a39a892b1dd713214db8b
penta2024 <nandari777@penta.co.kr>
2024-05-17 13:38:47 +0900

    email 성공 업로드

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -12,10 +12,8 @@ with DAG(
 ) as dag:
     
       send_email_task = EmailOperator(
-        task_id='send_email_task',
-        to='nandari777@penta.co.kr' ,
-        subject='Airflow 성공메일' ,
-        html_content='Airflow 작업이 완료되었습니다'
+        task_id="send_email_task",
+        to="nandari777@penta.co.kr" ,
+        subject="Airflow 성공메일" ,
+        html_content="Airflow 작업이 완료되었습니다"
     )  
-    
-    
