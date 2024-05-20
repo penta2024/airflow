6e65f18765777f93ad582d5ff49295ca3871b40c
penta2024 <nandari777@penta.co.kr>
2024-05-20 11:29:20 +0900

    python operator 추가

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -11,7 +11,7 @@ with DAG(
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-   def select_fruit():
+   def select_fruit(): 
        fruit = [ 'APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
        rand_int = random.randint(0,3)
        print(fruit[rand_int])
