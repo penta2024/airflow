12efdcc7bbf8e39edb7c47364b1588586db2d3a5
penta2024 <nandari777@penta.co.kr>
2024-05-20 11:13:37 +0900

    공통외부함수 업로드

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,19 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+       
+   task_grt_sftp = PythonOperator (
+       task_id ='task_grt_sftp' ,
+       python_callable=get_sftp
+   )
+   
