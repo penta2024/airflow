99056c470de2bb288808199e868b10ae8990009a
penta2024 <nandari777@penta.co.kr>
2024-05-21 17:02:43 +0900

    python dp_kwargs 업로드

--- dags/dags_bash_with_templete.py
+++ dags/dags_bash_with_templete.py
@@ -12,17 +12,17 @@ with DAG(
 ) as dag:
     
  bash_t1 = BashOperator(
-     task_id ='bash_t1' ,
-     bash_command='echo "data_interval_end : {{data_interval_end}} "'
+     task_id      = 'bash_t1' ,
+     bash_command = 'echo "data_interval_end : {{data_interval_end}} "'
  )
  
  bash_t2 = BashOperator(
      task_id ='bash_t2' ,
      env={
          'START_DATE' : '{{data_interval_start | ds  }}' ,
-         'END_DATE'   : '{{data_interval_end | ds    }}'
+         'END_DATE'   : '{{data_interval_end   | ds  }}'
      } ,
-     bash_command='echo $START_DATE && echo $END_DATE'
+     bash_command = 'echo $START_DATE && echo $END_DATE'
  )
  
  bash_t1 >> bash_t2
