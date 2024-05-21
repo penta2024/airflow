b3b8b77f6581902522ac47b9840044107bff2270
penta2024 <nandari777@penta.co.kr>
2024-05-21 10:18:27 +0900

    python args 업로드

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,20 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from common.common_func import regist
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag : 
+
+ regist_t1 = PythonOperator(
+      task_t1 ='regist_t1' ,
+      python_callable=regist ,
+      op_args=['penta' , 'man' , 'kr' , 'seoul']
+  ) 
+  
+  regist_t1
