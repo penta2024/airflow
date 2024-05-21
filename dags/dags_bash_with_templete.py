444ebb5b300e06a1871796fba163fedfb1449a6e
penta2024 <nandari777@penta.co.kr>
2024-05-21 16:51:44 +0900

    bash_templete 업로드

---
+++ dags/dags_bash_with_templete.py
@@ -0,0 +1,29 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_templete",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+ bash_t1 = BashOperator(
+     task_id ='bash_t1' ,
+     bash_command='echo "data_interval_end : {{data_interval_end}} "'
+ )
+ 
+ bash_t2 = BashOperator(
+     task_id ='bash_t2' ,
+     env={
+         'START_DATE' : '{{data_interval_start | ds  }}' ,
+         'END_DATE'   : '{{data_interval_end | ds    }}'
+     } ,
+     bash_command='echo $START_DATE && $END_DATE'
+ )
+ 
+ bash_t1 >> bash_t2
+ 
