c8cb069c7e1c041296cf5cf128f458807891eee8
penta2024 <nandari777@penta.co.kr>
2024-05-22 13:14:36 +0900

    python_templete 업로드

--- dags/dags_python_templete.py
+++ dags/dags_python_templete.py
@@ -24,10 +24,6 @@ with DAG(
     @task(task_id='python_t2')
     def python_function2(**kwargs):  
         print(kwargs)
-        print( 'ds:' + kwargs['ds'])
-        print( 'ts:' + kwargs['ts'])
-        print( 'data_interval_start:' + str(kwargs['data_interval_start']))
-        print( 'data_interval_end  :' + str(kwargs['data_interval_end']))       
-        print( 'task_instance      :' + str(kwargs['ti']))  
+
           
     python_t1 >> python_function2
