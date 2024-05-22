a6173b76e48fe83ddb02574aa5aaeabcab738333
penta2024 <nandari777@penta.co.kr>
2024-05-22 13:10:34 +0900

    python templete 업로드

--- dags/dags_python_templete.py
+++ dags/dags_python_templete.py
@@ -8,7 +8,7 @@ with DAG(
     dag_id="dags_python_templete",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
-    catchup=True
+    catchup=False
 ) as dag : 
     
     def python_function1(start_date, end_date, **kwargs) :
@@ -20,15 +20,6 @@ with DAG(
         python_callable =  python_function1 ,
         op_kwargs = {'start_date' : '{{data_interval_start | ds}}'  , 'end_date' : '{{data_interval_end | ds}}' }  
     )
-    
-    @task(task_id='python_t2')
-    def python_function2(**kwargs):  
-        print(kwargs)
-        print( 'ds:' + kwargs['ds'])
-        print( 'ts:' + kwargs['ts'])
-        print( 'data_interval_start:' + str(kwargs['data_interval_start']))
-        print( 'data_interval_end  :' + str(kwargs['data_interval_end']))       
-        print( 'task_instance      :' + str(kwargs['ti']))  
-          
-    python_t1 >> python_function2
+
+    python_t1 
       
