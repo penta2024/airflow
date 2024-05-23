cd09a3da1336b02bb2385cbae7c76e41947a93a5
penta2024 <nandari777@penta.co.kr>
2024-05-23 10:21:29 +0900

    bash_macro1 업로드

--- dags/dags_bash_with_macro_eq1.py
+++ dags/dags_bash_with_macro_eq1.py
@@ -14,9 +14,9 @@ with DAG(
       bash_t1 = BashOperator(
         task_id="bash_t1",
         env = { 'START_DATE' : '{{  data_interval_start.in_timezone("Asia/Seoul") | ds}}' ,
-                'END_DATE  ' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'          
+                'END_DATE'   : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'          
         } ,
-        bash_command= 'echo "START DATE : $START_DATE" && echo "END_DATE : $END_DATE" '
+        bash_command= 'echo "START_DATE : $START_DATE" && echo "END_DATE : $END_DATE" '
     )  
           
 
--- dags/dags_bash_with_macro_eq2.py
+++ dags/dags_bash_with_macro_eq2.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_with_macro_eq2",
-    schedule="0 0 L * *",
+    schedule="10 0 L * 6#2",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
@@ -14,9 +14,9 @@ with DAG(
       bash_t2 = BashOperator(
         task_id="bash_t2",
         env = { 'START_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}'  ,
-                'END_DATE  ' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'          
+                'END_DATE'   : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'          
         } ,
-        bash_command= 'echo "START DATE : $START_DATE" && echo "END_DATE : $END_DATE" '
+        bash_command= 'echo "START_DATE : $START_DATE" && echo "END_DATE : $END_DATE" '
     )  
           
 
