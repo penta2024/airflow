c9ec473bf142ef88232ffcf81578f391a072b1af
penta2024 <nandari777@penta.co.kr>
2024-05-23 10:08:33 +0900

    bash_macro1 업로드

--- dags/dags_bash_with_macro_eq1.py
+++ dags/dags_bash_with_macro_eq1.py
@@ -13,8 +13,8 @@ with DAG(
       # START_DATE : 전월말일, END_DATE : 1일전
       bash_t1 = BashOperator(
         task_id="bash_t1",
-        env = { 'START_DATE' : '{{  data_inyerval_start.in_timezone("Asia/Seoul") | ds}}' ,
-                'END_DAT   ' : '{{ (data_inyerval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'          
+        env = { 'START_DATE' : '{{  data_interval_start.in_timezone("Asia/Seoul") | ds}}' ,
+                'END_DAT   ' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'          
         } ,
         bash_command= 'echo "START DATE : $START_DATE" && echo "END_DATE : $END_DATE" '
     )  
--- dags/dags_bash_with_macro_eq2.py
+++ dags/dags_bash_with_macro_eq2.py
@@ -13,8 +13,8 @@ with DAG(
       # START_DATE : 2주전 월요일, END_DATE : 2주전 토요일
       bash_t2 = BashOperator(
         task_id="bash_t2",
-        env = { 'START_DATE' : '{{ (data_inyerval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}'  ,
-                'END_DAT   ' : '{{ (data_inyerval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'          
+        env = { 'START_DATE' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}'  ,
+                'END_DAT   ' : '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'          
         } ,
         bash_command= 'echo "START DATE : $START_DATE" && echo "END_DATE : $END_DATE" '
     )  
