baf7638f6645dae3de8eb04b67bb3b2c2d73ba8b
penta2024 <nandari777@penta.co.kr>
2024-05-24 10:12:41 +0900

    python_xcom 업로드

--- dags/dags_python_with_xcom_eq2.py
+++ dags/dags_python_with_xcom_eq2.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_with_xcom_eq1",
+    dag_id="dags_python_with_xcom_eq2",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
