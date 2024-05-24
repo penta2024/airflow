05cfab732ba4805a4e1f63530b5cc8a196926cfc
penta2024 <nandari777@penta.co.kr>
2024-05-24 11:01:42 +0900

    python_macro1 업로드

--- dags/dags_python_with_xcom_eq2.py
+++ dags/dags_python_with_xcom_eq2.py
@@ -17,7 +17,7 @@ with DAG(
     @task(task_id='python_xcom_pull_1')
     def xcom_pull_1(**kwargs) :
        ti = kwargs['ti']
-       value1 = ti.xcom_pull_1(key='python_xcom_push_by_return' )
+       value1 = ti.xcom_pull(task_ids ='python_xcom_push_by_return' )
        print( 'xcom_pull 매서드로 직접 찾은 리텀값 :' + value1) 
        
     @task(task_id='python_xcom_pull_2')
