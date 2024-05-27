bf316c189626eaf0202427b51662661baf584ca5
penta2024 <nandari777@penta.co.kr>
2024-05-27 13:31:57 +0900

    bash_python_xcom 업로드

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -35,6 +35,12 @@ bash_push = BashOperator (
                  'echo PUSH_COMPLETE' 
 )
        
-
+@task( task_id = 'python_pull')
+def python_pull_xcom(**kwargs) :
+    ti = kwargs['ti']
+    status_value = ti.xcom_pull( key = "bash_pushed")
+    return_value = ti.xcom_pull( task_ids = "bash_push") 
+    print('status_value : ' + str(status_value))
+    print('return_value : ' + return_value)
     
-bash_push 
+bash_push >>  python_pull_xcom()
