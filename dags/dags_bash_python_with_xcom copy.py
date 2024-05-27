90e88ce5d4821d4934be05d9a79c8d0088bf3a26
penta2024 <nandari777@penta.co.kr>
2024-05-27 14:11:45 +0900

    bash_python_xcom 업로드

--- dags/dags_bash_python_with_xcom copy.py
+++ dags/dags_bash_python_with_xcom copy.py
@@ -5,7 +5,7 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_python_with_xcom",
+    dag_id="dags_bash_python_with_xcom_copy",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
