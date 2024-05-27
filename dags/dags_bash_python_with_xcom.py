eb0ca8d008cc152f77b5501cd6c2a2cd6d5cc7d0
penta2024 <nandari777@penta.co.kr>
2024-05-27 11:04:45 +0900

    bash_python_xcom 업로드

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -11,16 +11,16 @@ with DAG(
     catchup=False
 ) as dag:
     
-  @task(task_id =  'python_push')
+  @task(task_id =  'python_push' )
   def python_push_xcom() :
         result_dict = { 'status' : 'Good' , 'data' : [1,2,3] , 'option_cnt' : 100}
         return result_dict
       
   bash_pull = BashOperator(
-        task_id="bash_pull",
-        env = { 'STATUS' : '{{ ti.xcom_pull( task_ids = "bash_push" )["status"]}}' ,
-                'DATA'   : '{{ ti.xcom_pull( task_ids = "bash_push" )["data"]}}' ,
-                'OPTION_CNT' : '{{ ti.xcom_pull( task_ids = "bash_push" )["option_cnt"]}}'
+        task_id= 'bash_pull' ,
+        env = { 'STATUS' : '{{ ti.xcom_pull( task_ids = "python_push" )["status"]}}' ,
+                'DATA'   : '{{ ti.xcom_pull( task_ids = "python_push" )["data"]}}' ,
+                'OPTION_CNT' : '{{ ti.xcom_pull( task_ids = "python_push" )["option_cnt"]}}'
                } ,
         bash_command= 'echo $STATUS && echo $DATA && echo &OPTION_CNT '
     )       
