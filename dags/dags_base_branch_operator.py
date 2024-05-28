ff80c27b25622d8fceb6f0a9d5fffd14c1a63d63
penta2024 <nandari777@penta.co.kr>
2024-05-28 13:22:48 +0900

    base_branch_operator 업로드

--- dags/dags_base_branch_operator.py
+++ dags/dags_base_branch_operator.py
@@ -25,27 +25,27 @@ with DAG(
         elif selected_item in ['B','C'] :
            return ['task_b','task_c']
    
-custom_branch_operator = CustomBranchOperator ( task_id = 'python_branch_task' )
+ custom_branch_operator = CustomBranchOperator(task_id = 'python_branch_task')
    
-def common_func(**kwargs) :
+ def common_func(**kwargs) :
         print(kwargs['selected'])
    
-task_a = PythonOperator (
+ task_a = PythonOperator (
         task_id = 'task_a' ,
         python_callable = common_func ,
         op_kwargs = { 'selected' : 'A'}
     ) 
  
-task_b = PythonOperator (
+ task_b = PythonOperator (
         task_id = 'task_b' ,
         python_callable = common_func ,
         op_kwargs = { 'selected' : 'B'}
     ) 
    
-task_c = PythonOperator (
+ task_c = PythonOperator (
         task_id = 'task_c' ,
         python_callable = common_func ,
         op_kwargs = { 'selected' : 'C'}
     ) 
    
-custom_branch_operator >> [ task_a, task_b, task_c ]
+ custom_branch_operator >> [ task_a, task_b, task_c ]
