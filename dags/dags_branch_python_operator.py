d0613118d79fa1d61360c7ffc9cdd54c4dfc4b54
penta2024 <nandari777@penta.co.kr>
2024-05-28 10:10:59 +0900

    branch_python_operator 업로드

---
+++ dags/dags_branch_python_operator.py
@@ -0,0 +1,52 @@
+from airflow.models.dag import DAG
+from datetime import datetime
+import pendulum
+
+from airflow.operators.python import PythonOperator
+from airflow.operators.python import BranchPythonOperator
+
+
+with DAG(
+    dag_id="dags_branch_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    def select_random() :
+        import random
+       
+        item_list = [ 'A', 'B', 'C' ]
+        selected_item = random.choice(item_list)
+        if selected_item == 'A' :
+           return 'task_a'
+        elif selected_item in ['B','C'] :
+           return ['task_b','task_c']
+       
+    python_branch_task = BranchPythonOperator (
+       task_id ='python_branch_task' ,
+       python_callable = select_random
+   )
+   
+    def common_func(**kwargs) :
+        print(kwargs['selectde'])
+   
+    task_a = PythonOperator (
+        task_id = 'task_a' ,
+        python_callable = common_func ,
+        op_kwargs = { 'selectde' : 'A'}
+    ) 
+ 
+    task_b = PythonOperator (
+        task_id = 'task_b' ,
+        python_callable = common_func ,
+        op_kwargs = { 'selectde' : 'B'}
+    ) 
+   
+    task_c = PythonOperator (
+        task_id = 'task_c' ,
+        python_callable = common_func ,
+        op_kwargs = { 'selectde' : 'C'}
+    ) 
+   
+    python_branch_task >> [ task_a, task_b, task_c ]
