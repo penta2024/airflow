b3407f6778a3c0431b53f2bbd8843801717d2a57
penta2024 <nandari777@penta.co.kr>
2024-05-28 14:05:29 +0900

    base_branch_operator 업로드

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -11,12 +11,12 @@ with DAG(
     catchup=False
 ) as dag:
     
-@task(task_id =  'python_push' )
-def python_push_xcom() :
+    @task(task_id =  'python_push' )
+    def python_push_xcom() :
         result_dict = { 'status' : 'Good' , 'data' : [1,2,3] , 'options_cnt' : 100 }
         return result_dict
       
-bash_pull = BashOperator(
+    bash_pull = BashOperator(
         task_id= 'bash_pull' ,
         env = { 'STATUS' : '{{ ti.xcom_pull( task_ids = "python_push" )["status"]}}' ,
                 'DATA'   : '{{ ti.xcom_pull( task_ids = "python_push" )["data"]}}' ,
@@ -25,22 +25,22 @@ bash_pull = BashOperator(
         bash_command = 'echo $STATUS && echo $DATA && echo $OPTIONS_CNT '
     )       
 
-python_push_xcom() >> bash_pull  
+    python_push_xcom() >> bash_pull  
 
 
-bash_push = BashOperator (
-  task_id = 'bash_push' ,
-  bash_command = 'echo PUSH_START '
-                 '{{ ti.xcom_push(key = "bash_pushed" , value = 200) }} && '
-                 'echo PUSH_COMPLETE' 
-)
+    bash_push = BashOperator (
+      task_id = 'bash_push' ,
+      bash_command = 'echo PUSH_START '
+                     '{{ ti.xcom_push(key = "bash_pushed" , value = 200) }} && '
+                     'echo PUSH_COMPLETE' 
+    )
        
-@task( task_id = 'python_pull')
-def python_pull_xcom(**kwargs):
-    ti = kwargs['ti']
-    status_value = ti.xcom_pull(key = "bash_pushed")
-    return_value = ti.xcom_pull(task_ids = 'bash_push') 
-    print('status_value : ' + str(status_value))
-    print('return_value : ' + return_value)
+    @task( task_id = 'python_pull')
+    def python_pull_xcom(**kwargs):
+        ti = kwargs['ti']
+        status_value = ti.xcom_pull(key = "bash_pushed")
+        return_value = ti.xcom_pull(task_ids = 'bash_push') 
+        print('status_value : ' + str(status_value))
+        print('return_value : ' + return_value)
     
-bash_push >>  python_pull_xcom()
+    bash_push >>  python_pull_xcom()
