1cd706bfd3c1aeb3a9fb16d070787d6c6bf856fa
penta2024 <nandari777@penta.co.kr>
2024-05-28 15:21:28 +0900

    base_branch_operator 업로드

--- dags/dags_python_with_trigger_rule_eg2.py
+++ dags/dags_python_with_trigger_rule_eg2.py
@@ -7,7 +7,7 @@ from airflow.operators.python import PythonOperator
 from airflow.exceptions       import AirflowException
 
 with DAG(
-    dag_id="dags_python_eith_trigger_rule_eg2",
+    dag_id="dags_python_with_trigger_rule_eg2",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
