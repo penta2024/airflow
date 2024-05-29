e74c7f451067057ce437164d085c5f75f0440282
penta2024 <nandari777@penta.co.kr>
2024-05-29 15:51:56 +0900

    trigger_dag_run 업로드

---
+++ dags/dags_trigger_dag_run_operator.py
@@ -0,0 +1,34 @@
+# Package Import
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+
+from airflow.operators.bash           import BashOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.edgemodifier       import Label
+
+with DAG(
+    dag_id="dags_trigger_dag_run_operator",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    start_task = BashOperator(
+        task_id = "start_task",
+        bash_command = "echo start_task"
+    )  
+      
+    trigger_dag_task = TriggerDagRunOperator (
+      task_id = 'trigger_dag_task' ,
+      trigger_dag_id = 'dags_python_operator'  ,
+      trigger_run_id = None ,
+      execution_date = {'data_interval_start'} ,
+      reset_dag_run = True ,
+      wait_for_completion = False ,
+      poke_interval = 60 ,
+      allowd_status = ['success'] ,
+      failed_status = None
+    )
+
+    start_task >> Label['트리거'] >>trigger_dag_task
