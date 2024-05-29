c3126e565d09122417a404e4b5f3e625788d4029
penta2024 <nandari777@penta.co.kr>
2024-05-29 16:05:51 +0900

    trigger_dag_run 업로드

--- dags/dags_trigger_dag_run_operator.py
+++ dags/dags_trigger_dag_run_operator.py
@@ -23,7 +23,7 @@ with DAG(
       task_id = 'trigger_dag_task' ,
       trigger_dag_id = 'dags_python_operator'  ,
       trigger_run_id = None ,
-      execution_date = {{'data_interval_start'}} ,
+      execution_date = '{{data_interval_start}}' ,
       reset_dag_run = True ,
       wait_for_completion = False ,
       poke_interval = 60 ,
