edfb6d1cf9d1b1cb93b2ba13c5c1225a51cca535
penta2024 <nandari777@penta.co.kr>
2024-05-30 11:11:57 +0900

    simple_http_operator 업로드

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,40 @@
+# Package Import
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+
+from airflow.operators.bash           import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators               import task
+from airflow.utils.edgemodifier       import Label
+
+with DAG(
+    dag_id="dags_simple_http_operator",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    '''  서울시 공공데이터 정보 '''
+    tb_cycle_station_info = SimpleHttpOperator(
+        task_id = 'tb_cycle_station_info'  ,
+        http_conn_if = 'openapi.seoul.go.kr'  ,
+        endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/CardSubwayStatsNew/1/10' ,
+        method = 'GET' ,
+        headers = {'Content-Type' : 'application/json' ,
+                   'charset' : 'utf-8' ,
+                   'Accept' : '*/*'
+                   }
+    )  
+      
+    @task( task_id = 'python_2')
+    def python_2(**kwargs) :
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull( task_ids ='tb_cycle_station_info')
+        import json
+        from pprint import pprint
+        
+        pprint(json.loads(rslt))
+    
+
+    tb_cycle_station_info >> Label('공공데이터') >> python_2
