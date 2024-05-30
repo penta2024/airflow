4a6f26247b7e0df633eb5adce135494c20bc1e7b
penta2024 <nandari777@penta.co.kr>
2024-05-30 15:34:44 +0900

    simple_http_operator 업로드

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -18,7 +18,7 @@ with DAG(
     tb_cycle_station_info = SimpleHttpOperator(
         task_id = 'tb_cycle_station_info'  ,
         http_conn_id = 'openapi.seoul.go.kr'  ,
-        endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10' ,
+        endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json//eduCmpltEnt/1/10' ,
         method = 'GET' ,
         headers = {'Content-Type' : 'application/json' ,
                    'charset' : 'utf-8' ,
@@ -33,7 +33,7 @@ with DAG(
         import json
         from pprint import pprint
         
-        pprint(json.loads(rslt))  # loads error 발생
+        pprint(json.loads(rslt))  
         print('aaa')
     
 
