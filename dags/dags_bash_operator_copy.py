d7af46a1c8f2b8a98f8a8ae4d072c69694ae107c
penta2024 <nandari777@penta.co.kr>
2024-06-05 16:09:59 +0900

    uploads

--- dags/dags_bash_operator_copy.py
+++ dags/dags_bash_operator_copy.py
@@ -5,7 +5,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_bash_operator_copy",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
