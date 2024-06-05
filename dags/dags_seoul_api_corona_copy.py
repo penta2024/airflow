c127f77e2f254a11f29e6751d3fe21079c097b94
penta2024 <nandari777@penta.co.kr>
2024-06-05 14:53:38 +0900

    uploads

--- dags/dags_seoul_api_corona_copy.py
+++ dags/dags_seoul_api_corona_copy.py
@@ -3,10 +3,11 @@ from airflow.models.dag import DAG
 import datetime
 import pendulum
 
+
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_seoul_api_corona_copy",
+    dag_id="dags_seoul_api_corona",
     schedule="0 7 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
@@ -14,8 +15,8 @@ with DAG(
     
          
     bash_t1 = BashOperator(
-        task_id="bash_t1",  
+        task_id="bash_t1",
         bash_command="echo whoami",
     )  
       
-    bash_t1
+    bash_t1  
