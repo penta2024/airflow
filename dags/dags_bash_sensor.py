5ece753717aea8d1955d5a1aebc28ef38c22db9f
penta2024 <nandari777@penta.co.kr>
2024-06-12 16:05:14 +0900

    bash_sensor 업로드

--- dags/dags_bash_sensor.py
+++ dags/dags_bash_sensor.py
@@ -14,7 +14,7 @@ with DAG(
     
       sensor_task_by_poke = BashSensor(
         task_id= 'sensor_task_by_poke',
-        env = {'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/ tvCorona19VaccinestatNew.csv'} , 
+        env = {'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'} , 
         bash_command= f'''echo $FILE &&
                           if [ -f $FILE ] ; then
                               exit 0
@@ -29,7 +29,7 @@ with DAG(
 
       sensor_task_by_reschedule = BashSensor(
         task_id= 'sensor_task_by_reschedule',
-        env = {'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/ tvCorona19VaccinestatNew.csv'} , 
+        env = {'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'} , 
         bash_command= f'''echo $FILE &&
                           if [ -f $FILE ] ; then
                               exit 0
@@ -44,7 +44,7 @@ with DAG(
       
       bash_task = BashOperator(
         task_id='bash_task',
-        env = {'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}} /tvCorona19VaccinestatNew.csv'} , 
+        env = {'FILE' : '/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv'} , 
 #        bash_command= 'echo "건수: 'cat $FILE | wc -l '"' ,
         bash_command= 'echo cat $FILE | wc -l' ,   
       )   
