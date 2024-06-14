4309a5ad14f42de6b3267e915d0cf3d616839900
penta2024 <nandari777@penta.co.kr>
2024-06-14 14:56:48 +0900

    file-sensor 업로드

---
+++ dags/dags_file_sensor.py
@@ -0,0 +1,28 @@
+import datetime
+import pendulum
+
+from airflow.models.dag           import DAG
+from airflow.sensors.filesystem   import FileSensor
+
+with DAG(
+    dag_id= 'dags_file_sensor',
+    schedule="0 7 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+      tvCorona19VaccinestatNew_sensor = FileSensor(
+        task_id= 'tvCorona19VaccinestatNew_sensor',
+        fs_conn_id = 'conn_file_opt_airflow_files' ,
+        filepath = 'tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv' , 
+        recusive = False,
+        poke_interval = 30,     #30 sec
+        timeout = 60*60*24 ,    #1  day
+        mode = 'reschedule' ,
+        soft_fail = False
+      )  
+
+
+      tvCorona19VaccinestatNew_sensor
+ 
+       
