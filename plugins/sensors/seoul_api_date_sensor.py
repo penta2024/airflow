dffdc0009a98c02949aae43ca8d8c7052cc5e6cc
penta2024 <nandari777@penta.co.kr>
2024-06-17 16:43:21 +0900

    custom_task_sensor 업로드

--- plugins/sensors/seoul_api_date_sensor.py
+++ plugins/sensors/seoul_api_date_sensor.py
@@ -11,7 +11,7 @@ import pandas as pd
 class SeoulApiDateSensor (BaseSensorOperator):
     template_fields = ('endpoint')
         
-    def __init__(self, dataset_nm, path, base_dt_col, day_off=0,  **kwargs) :
+    def __init__(self, dataset_nm, base_dt_col, day_off=0,  **kwargs) :
         '''
         dateset_nm : 서울시 공공데이터 포털에서 센싱하고자 하는 데이터셋 명
         base_dt_col : 센싱 기준 컬럼 ( yyyy.mm.dd...or yyyy/mm/dd... 형태민 가능)
@@ -19,7 +19,6 @@ class SeoulApiDateSensor (BaseSensorOperator):
         '''
         super().__init__(**kwargs)
         self.http_conn_id = 'openapi.seoul.go.kr'
-        self.path = path
         self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm + '/1/100'
         self.base_dt_col = base_dt_col
         self.day_off = day_off
@@ -37,7 +36,7 @@ class SeoulApiDateSensor (BaseSensorOperator):
         contents = json.loads(response.text)
         key_nm = list(contents.keys())[0]
         row_data = contents.get(key_nm).get('row')
-        last_dt = row_data[0].get(base_dt_col)
+        last_dt = row_data[0].get(self.base_dt_col)
         last_date = last_dt[:10]
         last_date = last_date.replace('.' , '-').replace('/' , '-')
         search_ymd = ( context.get('data_interval_end').in_timezone('Asia/Seoul') +relativedelta(days=self.day_off)).strftime('%Y-%m-%d')
