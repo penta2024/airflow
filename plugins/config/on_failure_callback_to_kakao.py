cea108d386d9636f3f87f2e4ffc0f82010eed843
penta2024 <nandari777@penta.co.kr>
2024-06-27 13:43:28 +0900

    on_failure_callback_kakao 업로드

--- plugins/config/on_failure_callback_to_kakao.py
+++ plugins/config/on_failure_callback_to_kakao.py
@@ -6,7 +6,7 @@ def on_failure_callback_to_kakao(context) :
     dag_id  = ti.dag_id
     task_id = ti.task_id
  #   err_msg = context.get('execption')
-    batch_date = context.get('data_interval_end').in_timezone('Asia/Seoul')
+    data_interval_end = context.get('data_interval_end').in_timezone('Asia/Seoul')
     
-    content = {f'{dag_id}.{task_id}' : f'에러내용 :{exception}', '':''  }   #content 길이는 2이상잉야 함
+    content = {f'{dag_id}.{task_id}' : f'에러내용 :{exception}', '':''  }                #content 길이는 2이상잉야 함
     send_kakao_msg(talk_title=f'task 실패 알람({data_interval_end})' , content=content)
